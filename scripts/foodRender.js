e0206705ddf411571d546fa71257708fe12e4005
conlibra <114357265+conlibra@users.noreply.github.com>
2024-08-10 21:48:19 -0700

    Add files via upload

---
+++ scripts/foodRender.js
@@ -0,0 +1,5 @@
+import { renderImages } from "./common.js";
+
+const numberOfFoodPhotos = 45;
+
+renderImages(numberOfFoodPhotos, "food");
---
+++ scripts/portraitsRender.js
@@ -0,0 +1,5 @@
+import { renderImages } from "./common.js";
+
+const numberOfPortraits = 90;
+
+renderImages(numberOfPortraits, "portraits");
